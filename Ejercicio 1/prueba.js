235d1f8433e5df9dd2a06c56fbeebba037fbdc26
James-Gamboa <jjguevarag@gmail.com>
2022-06-18 15:11:16 -0600

    hola

--- Ejercicio 1/prueba.js
+++ Ejercicio 1/prueba.js
@@ -1,13 +1,21 @@
-let color = prompt("Digite el color del semaforo");
+let edad = prompt("¿Cuál es tu edad?");
 
-if (color == "rojo") {
-  alert("ALTO");
-} else if (color == "amarillo") {
-  alert("PRECAUCION");
-} else if (color == "verde") {
-  alert("SIGA");
-} else {
-  alert(
-    "NO SE INGRESO LOS VALORES CORRECTO, POR FAVOR RECARGUE LA PAGINA Y ESCRIBA VERDE, AMARILLO, ROJO"
-  );
+// @ts-ignore
+edad = Number(edad);
+
+// @ts-ignore
+if (edad >= 0 && edad < 3) {
+  alert("Eres un bebe");
+  // @ts-ignore
+} else if (edad >= 4 && edad < 12) {
+  alert("Eres un niño");
+  // @ts-ignore
+} else if (edad >= 12 && edad < 18) {
+  alert("Eres un adolescente");
+  // @ts-ignore
+} else if (edad >= 18 && edad < 65) {
+  alert("Eres un adulto");
+  // @ts-ignore
+} else if (edad == 65 && edad < 65) {
+  alert("Eres un adulto mayor");
 }
