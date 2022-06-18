a6184d351074efb28989bd06d5c598c2498b1a36
James-Gamboa <jjguevarag@gmail.com>
2022-06-18 15:23:47 -0600

    hola

--- Ejercicio 3/prueba.js
+++ Ejercicio 3/prueba.js
@@ -1,13 +1,34 @@
-let color = prompt("Digite el color del semaforo");
+let pais = prompt("Ingrese el nombre de su pais");
+alert(pais?.toUpperCase().replace(/ /g,""));
 
-if (color == "rojo") {
-  alert("ALTO");
-} else if (color == "amarillo") {
-  alert("PRECAUCION");
-} else if (color == "verde") {
-  alert("SIGA");
-} else {
-  alert(
-    "NO SE INGRESO LOS VALORES CORRECTO, POR FAVOR RECARGUE LA PAGINA Y ESCRIBA VERDE, AMARILLO, ROJO"
-  );
-}
+if (pais == "costa rica"){
+  alert("Este es un pais America")
+} else if (pais == "argentina"){
+  alert("Este es un pais America")
+}  else if (pais == "colombia"){
+  alert("Este es un pais America")
+}  else if (pais == "nicaragua"){
+  alert("Este es un pais America")
+}  else if (pais == "panamá"){
+  alert("Este es un pais America")
+}  else if (pais == "uruguay"){
+  alert("Este es un pais America")
+}  else if (pais == "estados unidos"){
+  alert("Este es un pais America")
+}  else if (pais == "canadá"){
+  alert("Este es un pais America")
+}  else if (pais == "méxico"){
+  alert("Este es un pais America")
+}  else if (pais == "españa"){
+  alert("Este es un pais europa")
+}   else if (pais == "italia"){
+  alert("Este es un pais europa")
+}   else if (pais == "francia"){
+  alert("Este es un pais europa")
+}   else if (pais == "rumania"){
+  alert("Este es un pais europa")
+}   else if (pais == "inglaterra"){
+  alert("Este es un pais europa")
+}   else if (pais == "alemania"){
+  alert("Este es un pais europa")
+} 
