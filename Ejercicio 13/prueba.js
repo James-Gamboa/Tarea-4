c3f33b83417ac1765ad805b21c005842bd514490
James-Gamboa <jjguevarag@gmail.com>
2022-06-19 11:38:18 -0600

    hola

--- Ejercicio 13/prueba.js
+++ Ejercicio 13/prueba.js
@@ -1,13 +1,24 @@
-let saludar = prompt("Digite un buenos dias,buenas tardes o buenas noches");
+let futbol = prompt("Digite un jugador de futbol");
 
-if (saludar == "buenos dias") {
-  alert("buenos dias");
-} else if (saludar == "buenas tardes") {
-  alert("buenas tardes");
-} else if (saludar == "buenas noches") {
-  alert("buenas noches");
-} else {
-  alert(
-    "NO SE INGRESO LOS VALORES CORRECTO, POR FAVOR RECARGUE LA PAGINA Y ESCRIBA BUENOS DIAS, BUENAS TARDES, BUENAS NOCHES"
-  );
+if (futbol == "Messi") {
+  alert("el mejor del mundo");
+} else if (futbol == "Cristiano Ronaldo") {
+  alert("el mejor del mundo");
+} else if (futbol == "Maradona") {
+  alert("el mejor jugador del mubdo");
+} else if (futbol == "Robert Lewandowski"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Mohamed Salah"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Kylian Mbappe"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Erling Haaland "){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Karim Benzema"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Neymar"){
+  alert("no es el mejor jugador del mundo")
+} 
+else {
+  alert("NO SE INGRESO LOS VALORES CORRECTO, POR FAVOR RECARGUE LA PAGINA Y ESCRIBA A UN JUGADOR DE FÚTBOL");
 }
--- Ejercicio 14/prueba.js
+++ Ejercicio 14/prueba.js
@@ -1,13 +1,9 @@
-let saludar = prompt("Digite un buenos dias,buenas tardes o buenas noches");
-
-if (saludar == "buenos dias") {
-  alert("buenos dias");
-} else if (saludar == "buenas tardes") {
-  alert("buenas tardes");
-} else if (saludar == "buenas noches") {
-  alert("buenas noches");
-} else {
-  alert(
-    "NO SE INGRESO LOS VALORES CORRECTO, POR FAVOR RECARGUE LA PAGINA Y ESCRIBA BUENOS DIAS, BUENAS TARDES, BUENAS NOCHES"
-  );
+let numero ;
+numero = prompt("Introduzca su numero");
+// @ts-ignore
+if(numero % 2 == 0){
+  alert("EL número "+ numero +" es par");
+}else{
+  alert("EL número "+ numero +" es impar");
 }
+
