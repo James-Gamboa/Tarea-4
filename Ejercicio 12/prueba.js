bf5c99821dae6cfccccd82ddbcf82d8501f1ea06
James-Gamboa <jjguevarag@gmail.com>
2022-06-19 12:08:30 -0600

    hola

---
+++ Ejercicio 12/prueba.js
@@ -0,0 +1,24 @@
+let futbol = prompt("Digite un jugador de futbol");
+
+if (futbol == "Messi") {
+  alert("el mejor del mundo");
+} else if (futbol == "Cristiano Ronaldo") {
+  alert("el mejor del mundo");
+} else if (futbol == "Maradona") {
+  alert("el mejor jugador del mubdo");
+} else if (futbol == "Robert Lewandowski"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Mohamed Salah"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Kylian Mbappe"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Erling Haaland "){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Karim Benzema"){
+  alert("no es el mejor jugador del mundo")
+}  else if (futbol == "Neymar"){
+  alert("no es el mejor jugador del mundo")
+} 
+else {
+  alert("NO SE INGRESO LOS VALORES CORRECTO, POR FAVOR RECARGUE LA PAGINA Y ESCRIBA A UN JUGADOR DE FÚTBOL");
+}
--- Ejercicio 14/prueba.js
+++
@@ -1,9 +0,0 @@
-let numero ;
-numero = prompt("Introduzca su numero");
-// @ts-ignore
-if(numero % 2 == 0){
-  alert("EL número "+ numero +" es par");
-}else{
-  alert("EL número "+ numero +" es impar");
-}
-
